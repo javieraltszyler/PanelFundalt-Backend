4081ee12ad09e63d3edd16ddf09cd431e7da018e
Juan Bianchini <bianchinijuanit@gmail.com>
2025-04-15 11:30:32 -0300

    Fixed settings for Cloud Run

--- config/env.py
+++
@@ -1,2 +0,0 @@
-from config.settings.production import *
-
--- config/settings.py
+++ config/settings.py
@@ -1,12 +1,28 @@
 from pathlib import Path
-import environ
 import os
+from dotenv import load_dotenv
 from datetime import timedelta
 
+# Determinar el entorno
+DJANGO_ENV = os.getenv('DJANGO_ENV', 'env_dev')
 
-BASE_DIR = Path(__file__).resolve().parent.parent.parent
+# Cargar el archivo .env específico para el entorno
+if os.path.exists(f'.env.{DJANGO_ENV}'):
+    load_dotenv(f'.env.{DJANGO_ENV}')
+else:
+    load_dotenv()  # Cargar el .env predeterminado como respaldo
 
-env = environ.Env()
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+SECRET_KEY = os.getenv('SECRET_KEY', 'dlsakjadsjlkjdasladskjl1020')
+
+
+DEBUG = DJANGO_ENV == 'env_dev'
+
+
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', default=[])
 
 
 INSTALLED_APPS = [
@@ -64,29 +80,50 @@ TEMPLATES = [
     },
 ]
 
+
+
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=15),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
+    'ROTATE_REFRESH_TOKENS': True,
+    'BLACKLIST_AFTER_ROTATION': True,
+    'AUTH_HEADER_TYPES': ('Bearer',),
+    'AUTH_TOKEN_CLASSES': ('rest_framework_simplejwt.tokens.AccessToken',),
+}
+
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
 WSGI_APPLICATION = 'config.wsgi.application'
 
 
-# https://docs.djangoproject.com/en/5.1/ref/settings/#databases
+# Database
+# https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-
-# Password validation
-# https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
-
-AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
-]
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT', '5432'),
+    }
+}
 
 
 
@@ -124,27 +161,6 @@ CORS_ALLOW_HEADERS = [
     'x-requested-with',
 ]
 
-
-CORS_PREFLIGHT_MAX_AGE = 86400  # 24 horas
-
-
-# Internationalization
-# https://docs.djangoproject.com/en/5.1/topics/i18n/
-
-LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'UTC'
-
-USE_I18N = True
-
-USE_TZ = True
-
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.1/howto/static-files/
-
-STATIC_URL = 'static/'
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
@@ -180,4 +196,23 @@ SIMPLE_JWT = {
     "ROTATE_REFRESH_TOKENS": True,
     "BLACKLIST_AFTER_ROTATION": True,
 }
+
+
+
+# Internationalization
+# https://docs.djangoproject.com/en/5.2/topics/i18n/
+
+LANGUAGE_CODE = 'es-ar'
+
+TIME_ZONE = 'America/Argentina/Buenos_Aires'
+
+USE_I18N = True
+
+USE_TZ = True
+
+
+
+
+STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
